580bfc6c390aba232607299db6b0b2bb187101d3
Raysuner <1669232805@qq.com>
2021-09-25 22:54:13 +0800

    auth controller

--- src/controller/auth_controller.js
+++ src/controller/auth_controller.js
@@ -1,7 +1,18 @@
+const jwt = require("jsonwebtoken")
+const {PRIVATE_KEY} = require("../app/config")
+
 class AuthController {
     async login(ctx, next) {
-        const { name } = ctx.request.body
-        console.log(`welcome back ${name}`)
+        const { id, name } = ctx.user
+        const token = jwt.sign({ id, name }, PRIVATE_KEY, {
+            algorithm: "RS256",
+            expiresIn: 60 * 60 * 24 * 7
+        })
+        ctx.body = {id, name, token}
+    }
+
+    async authSuccess(ctx, next) {
+        ctx.body = "授权成功"
     }
 }
 
