ed449bf41da2b57d6c8608b948a54edc91afc9c4
Raysuner <1669232805@qq.com>
2021-09-25 22:55:29 +0800

    update

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,13 @@
 const Koa = require("koa")
 const bodyparser = require("koa-bodyparser")
-const userRouter = require("../router/user_router")
-const loginRouter = require("../router/auth_router")
+const useAllRoutes = require("../router/index")
 const { errorHandler } = require("./error_handler")
 
 const app = new Koa()
 
+app.useAllRoutes = useAllRoutes
 app.use(bodyparser())
-app.use(userRouter.routes())
-app.use(userRouter.allowedMethods()) // 判断请求方式是否被允许
-app.use(loginRouter.routes())
-app.use(loginRouter.allowedMethods())
+app.useAllRoutes()
 
 app.on("error", errorHandler)
 
