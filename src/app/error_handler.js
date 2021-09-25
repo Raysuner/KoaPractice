e9f935009e10b704c820da189a606ab7d2b2c33c
Raysuner <1669232805@qq.com>
2021-09-25 22:50:35 +0800

    update error handler

--- src/app/error_handler.js
+++ src/app/error_handler.js
@@ -11,6 +11,18 @@ function errorHandler(error, ctx) {
             status = 409
             message = "用户已经存在"
             break
+        case errorType.USER_NOT_EXIST:
+            status = 400
+            message = "用户不存在"
+            break
+        case errorType.PASSWORD_IS_NOT_CORRECT:
+            status = 400
+            message = "密码不正确"
+            break
+        case errorType.UNAUTHORIZATION:
+            status = 401
+            message = "未授权"
+            break
         default:
             status = 404
             message = "默认错误"
