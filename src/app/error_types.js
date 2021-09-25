0d7568f33aaccd7f0d92c40fae2e431f612a74cc
Raysuner <1669232805@qq.com>
2021-09-25 22:51:35 +0800

    update error types

--- src/app/error_types.js
+++ src/app/error_types.js
@@ -1,9 +1,15 @@
 const USERNAME_AND_PASSWORD_IS_REQUIRED = "username and password is required"
 const NOT_FOUND = "404 NOT FOUND"
 const USER_ALREADY_EXIST = "user_already_exist"
+const USER_NOT_EXIST = "user_not_exist"
+const PASSWORD_IS_NOT_CORRECT = "password_is_not_correct"
+const UNAUTHORIZATION = "unauthorization"
 
 module.exports = {
     USERNAME_AND_PASSWORD_IS_REQUIRED,
     NOT_FOUND,
     USER_ALREADY_EXIST,
+    USER_NOT_EXIST,
+    PASSWORD_IS_NOT_CORRECT,
+    UNAUTHORIZATION,
 }
