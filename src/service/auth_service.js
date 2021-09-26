61163cd01186fb0af3455ec5ab3eeae92b155e41
Raysuner <1669232805@qq.com>
2021-09-26 22:32:13 +0800

    add auth service

---
+++ src/service/auth_service.js
@@ -0,0 +1,16 @@
+const connections = require("../app/database")
+
+class AuthService {
+    async checkPermission(userId, commentId) {
+        const statement = `SELECT user_id from comment where id = ?`
+        try {
+            const [result] = await connections.execute(statement, [commentId])
+            const id = result[0].user_id
+            return id === userId
+        } catch (err) {
+            console.log(err)
+        }
+    }
+} 
+
+module.exports = new AuthService()
