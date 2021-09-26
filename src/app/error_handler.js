9a18177aa3b9fa48aa5bcfd4f0bd9b7da32c834a
Raysuner <1669232805@qq.com>
2021-09-26 22:28:35 +0800

    update error handle

--- src/app/error_handler.js
+++ src/app/error_handler.js
@@ -7,10 +7,6 @@ function errorHandler(error, ctx) {
             status = 400
             message = "账号或密码未填写"
             break
-        case errorType.USER_ALREADY_EXIST:
-            status = 409
-            message = "用户已经存在"
-            break
         case errorType.USER_NOT_EXIST:
             status = 400
             message = "用户不存在"
@@ -19,9 +15,29 @@ function errorHandler(error, ctx) {
             status = 400
             message = "密码不正确"
             break
+        case errorType.CAN_NOT_GET_CORRECT_PARAMS:
+            status = 400
+            message = "参数不正确"
+            break
         case errorType.UNAUTHORIZATION:
             status = 401
             message = "未授权"
+            break
+        case errorType.UNPERMISSION:
+            status = 401
+            message = "你没有这个权限"
+            break
+        case errorType.USER_ALREADY_EXIST:
+            status = 409
+            message = "用户已经存在"
+            break
+        case errorType.DATABASE_ERROR:
+            status = 500
+            message = "数据库出错"
+            break
+        case errorType.UPDATA_COMMENT_FAILED:
+            status = 500
+            message = "更新评论错误"
             break
         default:
             status = 404
--- src/app/error_types.js
+++ src/app/error_types.js
@@ -4,6 +4,11 @@ const USER_ALREADY_EXIST = "user_already_exist"
 const USER_NOT_EXIST = "user_not_exist"
 const PASSWORD_IS_NOT_CORRECT = "password_is_not_correct"
 const UNAUTHORIZATION = "unauthorization"
+const DATABASE_ERROR = "database_error"
+const CAN_NOT_GET_COMMENT_SIZE_WITH_QUERY = "can_not_get_comment_size_with_query"
+const CAN_NOT_GET_CORRECT_PARAMS = "can_not_get_correct_params"
+const UNPERMISSION = "unpermission"
+const UPDATA_COMMENT_FAILED = "update_comment_failed"
 
 module.exports = {
     USERNAME_AND_PASSWORD_IS_REQUIRED,
@@ -12,4 +17,9 @@ module.exports = {
     USER_NOT_EXIST,
     PASSWORD_IS_NOT_CORRECT,
     UNAUTHORIZATION,
+    DATABASE_ERROR,
+    CAN_NOT_GET_CORRECT_PARAMS,
+    CAN_NOT_GET_COMMENT_SIZE_WITH_QUERY,
+    UNPERMISSION,
+    UPDATA_COMMENT_FAILED,
 }
